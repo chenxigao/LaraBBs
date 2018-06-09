f2c9cc1fd8db458bbe72864f135a7ecfe808ba7d
vagrant <vagrant@homestead>
2018-06-09 16:55:35 +0000

    List Topics created by user

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -14,4 +14,33 @@ class Topic extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function scopeWithOrder($query,$order){
+
+        switch ($order){
+            case 'recent':
+                $query->recentReplied();
+                break;
+            default:
+                $query->recent();
+                break;
+
+        }
+        //预加载防止N+1问题
+        return $query->with('user','category');
+
+    }
+
+    public function scopeRecentReplied($query){
+
+        //当有新话题时，我们会编辑逻辑更改话题reply_count属性
+        //自动触发框架对updated_at时间戳更新
+        return $query->orderBy('updated_at','category');
+
+    }
+
+    public function scopeRecent($query){
+        //对创建时间进行排序
+        return $query->orderBy('created_at','desc');
+    }
+
 }
